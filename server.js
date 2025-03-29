e244e5df098d166e5770a4928b1bf9f2225b849b
Teremki16 <143702401+Teremki16@users.noreply.github.com>
2025-03-29 17:36:28 +0200

    style of messages

--- server.js
+++ server.js
@@ -1,3 +1,46 @@
 require("dotenv").config()
+let db=require("./db")//приєднали файл с базою даниз
+let http= require("http")
 
-console.log(process.env.HOST)
+// console.log(process.env.HOST)
+
+// db.query("SHOW TABLES", function(err,result){
+//     if(err){
+//         console.error(err)
+//     }else{
+//         console.log(result)
+//     }
+// })
+
+let path=require("path")
+let fs= require("fs")//express(basa danih)
+
+let pathToIndex=path.join(__dirname, "static", "index.html")
+let index = fs.readFileSync(pathToIndex, "utf-8")
+
+let pathToStyle=path.join(__dirname, "static", "style.css")
+let style = fs.readFileSync(pathToStyle, "utf-8")
+
+let pathToScript=path.join(__dirname, "static", "script.js")
+let script = fs.readFileSync(pathToScript, "utf-8")
+
+
+http.createServer((req, res)=>{
+    switch(req.url){
+        case "/":
+            res.writeHead(200, {"content-type": "text/html"})
+            res.end(index)
+            break;
+        case "/style.css":
+            res.writeHead(200, {"content-type": "text/css"})
+            res.end(style)
+            break;
+        case "/script.js":
+            res.writeHead(200, {"content-type": "text/js"})
+            res.end(script)
+            break;
+        default:
+            res.writeHead(404, {"content-type": "text/html"})
+            res.end("<h1>404 not found</h1>")
+    }
+}).listen(3000, ()=> console.log("server is on!"))
